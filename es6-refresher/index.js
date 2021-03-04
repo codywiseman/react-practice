92bc80a181235cf6e9ac598d32a8e67312ec4813
Cody Wiseman <cwiseman09@yahoo.com>
2021-03-04 23:21:11 +0000

    let/const and arrow function notes

---
+++ es6-refresher/index.js
@@ -0,0 +1,16 @@
+/**
+* * Let and Const
+* * Use let for variable values that will be reassigned
+* * Use const for constant values that are only assigned once
+*/
+
+/**
+ * * Arrow Functions
+ * * no more issues with 'this' keyword
+ *    * when using this inside of an arrow function, it will always keep its context
+ * * omit return keyword when using only one line of code
+ */
+
+ const myFunc = () => {
+   // code block
+ }
