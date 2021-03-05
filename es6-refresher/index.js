191a79f83ec62a8cdaa77e0581d3132e1ec77fbd
Cody Wiseman <cwiseman09@yahoo.com>
2021-03-05 00:11:17 +0000

    classes notes

--- es6-refresher/index.js
+++ es6-refresher/index.js
@@ -11,6 +11,46 @@
  * * omit return keyword when using only one line of code
  */
 
- const myFunc = () => {
-   // code block
- }
+const myFunc = () => {
+  // code block
+}
+
+ /**
+  * * Exports & Imports (Modules)
+  * * export default - only one per moudle (can change name when importing. No {} necessary )
+  * * named export - multiple named exports per module. use {} when importing (can change name using export { myFunction as functional })
+  */
+
+  /**
+   * * Classes
+   * * classes are essentially blueprints for object
+   * * can contain properties and methods
+   * * classes support inheritance - a class can inherit all properties and methods from another class while also adding its own
+   */
+
+class Human {
+  constructor() {
+    this.gender = 'male';
+  }
+  printGender() {
+    console.log(this.gender)
+  }
+}
+
+class Person extends Human{
+  /**
+   * properties are added using a constructor, a
+   * default method that is created when a class is instantiated
+  */
+  constructor() {
+    super(); // keyword that executes the parent constructor
+    this.name = 'Cody';
+  }
+  printMyName() {
+    console.log(this.name);
+  }
+}
+
+const person = new Person();
+person.printMyName();
+person.printGender();
