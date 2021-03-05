2b61e7b297bebd43349717dfe82cd7e19481a2be
Cody Wiseman <cwiseman09@yahoo.com>
2021-03-05 05:27:00 +0000

    styles with stylesheets

--- react-app/src/Person/Person.js
+++ react-app/src/Person/Person.js
@@ -1,11 +1,12 @@
 import React from 'react';
-
+import './Person.css';
 
 const person = (props) => {
 return (
-  <div>
+  <div className="Person">
     <p onClick={props.click}>Hi, my name is {props.name} and I am {props.age} years old!</p>
     <p>{props.children}</p>
+    <input type="text" onChange={props.changed} value={props.name} />
   </div>
   )
 }
