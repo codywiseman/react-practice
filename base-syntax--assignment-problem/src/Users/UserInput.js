560bb89a8c973b96daf8bdfa0b69d48ce12b1dec
Cody Wiseman <cwiseman09@yahoo.com>
2021-03-08 22:38:08 +0000

    assignment 1 updated

--- base-syntax--assignment-problem/src/Users/UserInput.js
+++ base-syntax--assignment-problem/src/Users/UserInput.js
@@ -4,7 +4,7 @@ import './users.css'
 const userInput = (props) => {
   return (
     <div className='text-center'>
-      <input onChange={props.changeUser} value={props.username}></input>
+      <input type='text' onChange={props.changeUser} value={props.username}></input>
     </div>
   )
 }
