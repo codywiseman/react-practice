744ac27a2c6b7e27ffd808da3eb493110b4b6be0
Cody Wiseman <cwiseman09@yahoo.com>
2021-03-09 20:55:26 +0000

    changed event handler

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,14 +1,119 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Cody', age: 26 },
+      { name: 'Allie', age: 30 },
+      { name: 'Nathan', age: 35 }
+    ],
+    showPersons: false
+  }
+  swithNameHandler = (newName) => {
+    this.setState({
+      persons: [
+        { name: 'Cody', age: 2 },
+        { name: newName, age: 30 },
+        { name: 'Nathan', age: 102 }
+      ]
+    })
+  }
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: 'Cody', age: 2 },
+        { name: 'Allie', age: 30 },
+        { name: event.target.value, age: 102 }
+      ]
+    })
+  }
+  togglePersonHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({ showPersons: !doesShow })
+  }
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    }
     return (
+       //* React converts the below JSX to HTML.
       <div className="App">
         <h1> Hi, I'm a React App</h1>
+        <button
+          onClick={this.togglePersonHandler}
+          style={style}>Switch Name
+        </button>
+        {
+          this.state.showPersons ?
+            <div>
+            <Person
+              name={this.state.persons[0].name}
+              age={this.state.persons[0].age} >My Hobbies: Coding</Person>
+            <Person
+              name={this.state.persons[1].name}
+              age={this.state.persons[1].age} />
+            <Person
+              name={this.state.persons[2].name}
+              age={this.state.persons[2].age}
+              click={this.swithNameHandler.bind(this, 'Allie!')}
+              changed={this.nameChangedHandler} />
+          </div>
+          : null
+        }
       </div>
+      // * JSX gets compiled into the React code below. That is why we import React. JSX code calls React.createElement()
+    //  React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App!!!'))
     );
   }
 }
 
 export default App;
+
+
+/*
+const app = props => {
+  // useState can be used as many times as you want
+  const [ personsState, setPersonsState ] = useState({
+    persons: [
+      { name: 'Cody', age: 26 },
+      { name: 'Allie', age: 30 },
+      { name: 'Nathan', age: 35 }
+    ]
+  })
+  const switchNameHandler = () => {
+    setPersonsState({
+      // Changing state this way does not automatically merge the other data in state
+      persons: [
+        { name: 'Cody', age: 2 },
+        { name: 'Allison', age: 30 },
+        { name: 'Nathan', age: 102 }
+      ]
+    })
+  }
+  return (
+      //* React converts the below JSX to HTML.
+    <div className="App">
+      <h1> Hi, I'm a React App</h1>
+      <button onClick={switchNameHandler}>Switch Name</button>
+      <Person
+        name={personsState.persons[0].name}
+        age={personsState.persons[0].age} >My Hobbies: Coding</Person>
+      <Person
+        name={personsState.persons[1].name}
+        age={personsState.persons[1].age}
+        click={switchNameHandler} />
+      <Person
+        name={personsState.persons[2].name}
+        age={personsState.persons[2].age} />
+    </div>
+    // * JSX gets compiled into the React code below. That is why we import React.
+  //  React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App!!!'))
+  );
+}
+*/
